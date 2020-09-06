cd07fe61ba05c13b3d7f33c4c319f3157a2853a0
shaikhmohdjaffer <shaikhmohdjaffer@gmail.com>
2020-09-06 13:27:22 +0000

    commit

--- Tools/Batch.bat
+++ Tools/Batch.bat
@@ -1,3 +1,5 @@
+echo off
+
 echo == Delete objects folder and changeLog file for LQBASE schema if it already exists ==
 
 cd C:\Users\opc\Desktop\SONY\Liquibase\LQBASE\
@@ -11,9 +13,3 @@ echo changeLog file deletion exit code:%ERRORLEVEL%
 echo == Generate a changeLog file with current state of the LQBASE schema ==
 
 liquibase --changeLogFile=C:\Users\opc\Desktop\SONY\Liquibase\LQBASE\LQBASE_changeLog.xml generateChangeLog
-echo ChangeLog generation exit code:%ERRORLEVEL% 
-
-echo Run DropAllObjects.sql to drop all objects from the LQBASE_CI_BUILD schema
-echo on
-Call exit | sqlplus LQBASE_CI_BUILD/LQBaseBuild##123@ltidbpdb Tools/DropAllObjects.sql
-echo off
