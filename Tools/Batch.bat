686f6227f63c52ec14dcfb5a01036821a2000c2f
shaikhmohdjaffer <shaikhmohdjaffer@gmail.com>
2020-09-06 12:49:39 +0000

    commit

--- Tools/Batch.bat
+++ Tools/Batch.bat
@@ -2,19 +2,12 @@ echo off
 
 echo == Delete objects folder and changeLog file for LQBASE schema if it already exists ==
 
-cd C:\Users\opc\Desktop\SONY\Liquibase\LQBASE
-
-rmdir /Q /S objects
-del LQBASE_changeLog.xml
-
-IF %ERRORLEVEL% NEQ 0 (
-echo objects folder and changeLog file does not exists
-)
-
+rmdir /Q /S C:\Users\opc\Desktop\SONY\Liquibase\LQBASE\objects
+del C:\Users\opc\Desktop\SONY\Liquibase\LQBASE\LQBASE_changeLog.xml
 
 echo == Generate a changeLog file with current state of the LQBASE schema ==
 
-liquibase --changeLogFile=LQBASE_changeLog.xml generateChangeLog
+liquibase --changeLogFile=C:\Users\opc\Desktop\SONY\Liquibase\LQBASE\LQBASE_changeLog.xml generateChangeLog
 
 
 
