96992af1c02a3ecac1743ec0af6202af88f17948
shaikhmohdjaffer <shaikhmohdjaffer@gmail.com>
2020-09-07 07:28:41 +0000

    updated buld1.bat file

--- Tools/Liquibase-CI-Build1.bat
+++ Tools/Liquibase-CI-Build1.bat
@@ -2,13 +2,11 @@ echo off
 
 echo == Delete objects folder and changeLog file for LQBASE schema if it already exists ==
 
-cd C:\Users\opc\Desktop\SONY\Liquibase\LQBASE\
-
-rmdir /Q /S objects
+rmdir /Q /S LQBASE/objects
 echo Folder deletion exit code:%ERRORLEVEL% 
 
-del LQBASE_changeLog.xml
+del LQBASE/LQBASE_changeLog.xml
 echo changeLog file deletion exit code:%ERRORLEVEL% 
 
 echo == Generate a changeLog file with current state of the LQBASE schema ==
-liquibase --changeLogFile=C:\Users\opc\Desktop\SONY\Liquibase\LQBASE\LQBASE_changeLog.xml generateChangeLog
+liquibase --defaultsFile=LQBASE/liquibase.properties --changeLogFile=LQBASE/LQBASE_changeLog.xml generateChangeLog
