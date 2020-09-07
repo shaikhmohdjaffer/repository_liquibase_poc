f73a48132e282a020984fff0d358675afe6a71eb
shaikhmohdjaffer <shaikhmohdjaffer@gmail.com>
2020-09-07 07:44:17 +0000

    commit

--- Tools/Liquibase-CI-Build1.bat
+++ Tools/Liquibase-CI-Build1.bat
@@ -2,11 +2,12 @@ echo off
 
 echo == Delete objects folder and changeLog file for LQBASE schema if it already exists ==
 
-rmdir /Q /S LQBASE/objects
+rmdir /Q /S objects
 echo Folder deletion exit code:%ERRORLEVEL% 
 
-del LQBASE/LQBASE_changeLog.xml
+del LQBASE_changeLog.xml
 echo changeLog file deletion exit code:%ERRORLEVEL% 
 
 echo == Generate a changeLog file with current state of the LQBASE schema ==
-liquibase --defaultsFile=LQBASE/liquibase.properties --changeLogFile=LQBASE/LQBASE_changeLog.xml generateChangeLog
+cd C:\Users\opc\Desktop\SONY\Liquibase\LQBASE
+liquibase --changeLogFile=LQBASE/LQBASE_changeLog.xml generateChangeLog
